8dc3af249ebc0dde3bdf1ddb757cdc222cc222af
erenerisken <erenerisken@gmail.com>
2019-08-28 16:28:39 +0300

    launch file added

--- ORCA/hectorquad/src/multipleGoPose.cpp
+++ ORCA/hectorquad/src/multipleGoPose.cpp
@@ -7,33 +7,14 @@ ros::ServiceClient *clientPtr1, *clientPtr2, *clientPtr3, *clientPtr4;
 bool serviceCallback(std_srvs::EmptyRequest &req, std_srvs::EmptyResponse &res)
     {
         ROS_INFO_STREAM("Multiple Go Pose Called.");
-        hectorquad::coordinateRequest req1;
-        hectorquad::coordinateResponse res1;
-        req1.x = 10.0;
-        req1.y = 10.0;
-        req1.z = 2.0;
-        clientPtr1->call(req1, res1);
         
-        hectorquad::coordinateRequest req2;
-        hectorquad::coordinateResponse res2;
-        req2.x = -10.0;
-        req2.y = -10.0;
-        req2.z = 2.0;
-        clientPtr2->call(req2, res2);
-        
-        hectorquad::coordinateRequest req3;
-        hectorquad::coordinateResponse res3;
-        req3.x = -10.0;
-        req3.y = 10.0;
-        req3.z = 2.0;
-        clientPtr3->call(req3, res3);
-        
-        hectorquad::coordinateRequest req4;
-        hectorquad::coordinateResponse res4;
-        req4.x = 10.0;
-        req4.y = -10.0;
-        req4.z = 2.0;
-        clientPtr4->call(req4, res4);
+        std_srvs::EmptyRequest tmpReq;
+        std_srvs::EmptyResponse tmpRes;
+        clientPtr1->call(tmpReq,tmpRes);
+        clientPtr2->call(tmpReq,tmpRes);
+        clientPtr3->call(tmpReq,tmpRes);
+        clientPtr4->call(tmpReq,tmpRes);
+
         ROS_WARN_STREAM("Service is done!");
     }
 
@@ -42,20 +23,20 @@ int main(int argc, char* argv[])
         ros::init(argc, argv, "multipleGoPose");
         ros::NodeHandle nh;
         
-        ros::service::waitForService("uav1/quadGoPose");
-        ros::ServiceClient client1 = nh.serviceClient<hectorquad::coordinate>("uav1/quadGoPose");
+        ros::service::waitForService("uav1/startPlanning");
+        ros::ServiceClient client1 = nh.serviceClient<std_srvs::Empty>("uav1/startPlanning");
         clientPtr1 = &client1;
 
-        ros::service::waitForService("uav2/quadGoPose");
-        ros::ServiceClient client2 = nh.serviceClient<hectorquad::coordinate>("uav2/quadGoPose");
+        ros::service::waitForService("uav2/startPlanning");
+        ros::ServiceClient client2 = nh.serviceClient<std_srvs::Empty>("uav2/startPlanning");
         clientPtr2 = &client2;
         
-        ros::service::waitForService("uav3/quadGoPose");
-        ros::ServiceClient client3 = nh.serviceClient<hectorquad::coordinate>("uav3/quadGoPose");
+        ros::service::waitForService("uav3/startPlanning");
+        ros::ServiceClient client3 = nh.serviceClient<std_srvs::Empty>("uav3/startPlanning");
         clientPtr3 = &client3;
         
-        ros::service::waitForService("uav4/quadGoPose");
-        ros::ServiceClient client4 = nh.serviceClient<hectorquad::coordinate>("uav4/quadGoPose");
+        ros::service::waitForService("uav4/startPlanning");
+        ros::ServiceClient client4 = nh.serviceClient<std_srvs::Empty>("uav4/startPlanning");
         clientPtr4 = &client4;
     
         ros::ServiceServer service = nh.advertiseService("multiGoPose", &serviceCallback);
